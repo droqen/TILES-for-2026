f727cd2c6f8981ab2ed839207fef5adc200b83c0
droqen <droqen@gmail.com>
2026-03-15 11:02:53 -0400

    bubbles bubbles bubbles

--- dreams/1a3e-birdhomes/birdplayer.gd
+++ dreams/1a3e-birdhomes/birdplayer.gd
@@ -4,12 +4,30 @@ const BIRDHOP_BEEP : String = &"https://www.beepbox.co/#9n11sbk0l00e00t43a7g00j0
 const POPSOUND_BEEP : String = &"https://www.beepbox.co/#9n11s0k0l00e00t2ma7g00j07r1i0o5T0v4u00f0qM012d04w1h0E0T2v1u02f10w4qw02d03w2E0b4gp1hIWXdVifgg54FMZ100"
 @onready var maze = $"../Maze"
 
+enum {RESPAWNBUF}
+
 func play_pop_sound() -> void:
 	Beeper.play_sfx(POPSOUND_BEEP)
+	
+	if is_instance_valid(maze):
+		var empty_cells = maze.get_used_cells_by_tids([0])
+		var respawn_cells = []
+		if empty_cells:
+			for _i in range(2):
+				var random_empty_cell = empty_cells[randi() % len(empty_cells)]
+				if not random_empty_cell in respawn_cells:
+					respawn_cells.append(random_empty_cell)
+		
+		for rcell in respawn_cells:
+			maze.set_cell_tid(rcell, [48,58][randi()%2])
+		await get_tree().create_timer(0.2).timeout
+		if is_instance_valid(maze):
+			for rcell in respawn_cells:
+				maze.set_cell_tid(rcell, [18,28,38][randi()%3])
 
 func _ready() -> void:
 	super._ready()
-	bufs.setup_bufons([LANDBUF,8,])
+	bufs.setup_bufons([LANDBUF,8,RESPAWNBUF,30,])
 
 func popbubble(cell:Vector2i) -> void:
 	play_pop_sound()
@@ -22,15 +40,18 @@ func _physics_process(_delta: float) -> void:
 	if maze.get_cell_tid(maze.local_to_map(position)) in [18,28,38]:
 		popbubble(maze.local_to_map(position))
 	if maze.get_cell_tid(maze.local_to_map(position)) == 29:
-		maze.set_cell_tid(maze.local_to_map(position), 0)
-		queue_free() # bye
-		return
+		if position.distance_to(maze.map_to_local(maze.local_to_map(position))) <3:
+			maze.set_cell_tid(maze.local_to_map(position), 0)
+			queue_free() # bye
+			return
 	var dpad := Pin.get_dpad()
 	var onfloor := is_on_floor()
 	var ducking := onfloor and (dpad.y>0 or bufs.has(LANDBUF))
 	if Pin.get_jump_hit(): bufs.on(JUMPBUF)
 	tow_vx(dpad.x, 1.0, 0.2 if onfloor else 0.1)
 	tow_gravity(2.3, 0.03, Pin.get_jump_held(), 0.08)
+	#if vy < 0.75 and (dpad.y > 0) and not onfloor and bufs.has(FLORBUF):
+		#vy += 0.25
 	apply_velocities()
 	if position.x < 3 and vx < 0:
 		position.x += 230-6
@@ -52,19 +73,28 @@ func _physics_process(_delta: float) -> void:
 		elif dpad.x:
 			spr.setup([13+d,14+d],8)
 		else:
-			spr.setup([10+d,10+d,11+d],15)
-		
+			spr.setup([10+d,10+d,10+d,11+d,11+d],44)
+	
+	if bufs.has(RESPAWNBUF):
+		spr.call("show" if bufs.read(RESPAWNBUF) % 6 < 3 else "hide")
+	else:
+		spr.show()
+	
 	if position.y > 164:
-		var beep = get_tree().get_first_node_in_group(NavdiBeeper.BEEPER_BEEP_GROUP)
-		print("beep? ",beep)
-		if beep and beep is NavdiBeep:
-			print("beep silenced")
-			(beep as NavdiBeep).url = ''
-		else:
-			var silence_beep = NavdiBeep.new()
-			silence_beep.url = ''
-			Dreamer.spawn(silence_beep)
-			print("created silence beep")
-		$"../Label".gradually_delete()
-		queue_free()
+		position = Vector2(135,96)
+		vx = 0
+		vy = -0.3
+		bufs.on(RESPAWNBUF)
+		#var beep = get_tree().get_first_node_in_group(NavdiBeeper.BEEPER_BEEP_GROUP)
+		#print("beep? ",beep)
+		#if beep and beep is NavdiBeep:
+			#print("beep silenced")
+			#(beep as NavdiBeep).url = ''
+		#else:
+			#var silence_beep = NavdiBeep.new()
+			#silence_beep.url = ''
+			#Dreamer.spawn(silence_beep)
+			#print("created silence beep")
+		#$"../Label".gradually_delete()
+		#queue_free()
 		
