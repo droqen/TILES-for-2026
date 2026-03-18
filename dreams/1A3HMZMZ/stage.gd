882d9d3d854bb6a4736daf714f3ed18632ef1a20
droqen <droqen@gmail.com>
2026-03-18 01:29:40 -0400

    maze

--- dreams/1A3HMZMZ/stage.gd
+++ dreams/1A3HMZMZ/stage.gd
@@ -2,7 +2,7 @@ extends Node2D
 @onready var vessel : NavdiVessel = $"../NavdiVessel"
 @onready var player := $minotaur
 @onready var maze := $Maze
-@export var roomcoords := Vector2i(1,0)
+@export var roomcoords := Vector2i(2,0)
 const ROOMSIZE := Vector2i(10,10)
 func loadroom() -> void:
 	maze.copy_from(
@@ -12,10 +12,14 @@ func loadroom() -> void:
 func _ready() -> void:
 	loadroom.call_deferred()
 func _physics_process(_delta: float) -> void:
-	var region : Rect2i = NavdiGenUtil.shrink_rect2i(
-			Rect2i(0,0,100,100), 1)
-	var oobdir := NavdiGenUtil.gen_oobdir(player.position, region)
-	if oobdir:
-		player.position -= oobdir * region.size * 0.98
-		roomcoords += oobdir
-		loadroom()
+	if is_instance_valid(player):
+		var region : Rect2i = NavdiGenUtil.shrink_rect2i(
+				Rect2i(0,0,100,100), 1)
+		var oobdir := NavdiGenUtil.gen_oobdir(player.position, region)
+		if oobdir:
+			player.position -= oobdir * region.size * 0.98
+			roomcoords += oobdir
+			loadroom()
+		
+		if maze.get_cell_tid(maze.local_to_map(player.position)) == 99:
+			player.queue_free()
