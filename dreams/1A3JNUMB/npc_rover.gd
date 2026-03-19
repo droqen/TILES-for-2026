2a8524254f240766353d049d8818b244d078bea0
droqen <droqen@gmail.com>
2026-03-19 18:39:58 -0400

    number devil - mars - pt2.

--- dreams/1A3JNUMB/npc_rover.gd
+++ dreams/1A3JNUMB/npc_rover.gd
@@ -3,13 +3,14 @@ extends Node2D
 const IS_ROVER = true
 
 @onready var label : Label = $Label
-var label_bottom_y : int = -1
+#var label_bottom_y : int = -1
+@onready var labelcolour : Color = label.modulate
 
 var typing : bool = false
 var untyping : bool = true
 
 func _ready() -> void:
-	label_bottom_y = label.get_visible_line_count()
+	#label_bottom_y = label.get_visible_line_count()
 	label.visible_characters = 0
 	$playertouch.area_entered.connect(func(_plr):
 		typing = true; untyping = false;
@@ -22,8 +23,13 @@ func _ready() -> void:
 	)
 func _physics_process(_delta: float) -> void:
 	if typing:
-		if label.visible_characters < len(label.text):
-			label.visible_characters += 1
-	if untyping:
+		if not label.visible or label.visible_characters != len(label.text):
+			label.show()
+			label.modulate = Color.WHITE
+			label.visible_characters = len(label.text)
+			await get_tree().create_timer(0.1).timeout
+			if is_instance_valid(label):
+				label.modulate = labelcolour
+	elif untyping:
 		if label.visible_characters > 0:
 			label.visible_characters -= 1
